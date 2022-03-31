f2b9ac653416e2e883378880e843ff9c26f84d9a
Maria Cardona <mechas8703@hotmail.com>
2022-03-31 18:21:00 -0400

    build

---
+++ client/build/precache-manifest.35ba398c20bccbecaffcb21af94af335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2a3f8832975815fa733bdd428d7b9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b306b2de5078c8a3f4d",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "25a93346abea45cccf2a",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "6b306b2de5078c8a3f4d",
+    "url": "/static/js/2.7300e291.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.7300e291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a93346abea45cccf2a",
+    "url": "/static/js/main.a539030d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
